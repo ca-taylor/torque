21bdc78a55f3de17b6c455f0bc4938fa91946e39
David Beer <dbeer@adaptivecomputing.com>
2014-04-04 14:17:54 -0600

    Fix a crash when a mom update is received and pbs_server is too busy to service it.

--- src/server/incoming_request.c
+++ src/server/incoming_request.c
@@ -134,14 +134,14 @@ int process_pbs_server_port(
 
       isr->chan = chan;
       isr->args = args;
-
-      // don't let this get cleaned up below
-      chan = NULL;
   
       if (threadpool_is_too_busy(request_pool, ATR_DFLAG_MGRD) == false)
         svr_is_request(isr);
       else
         write_tcp_reply(chan, IS_PROTOCOL, IS_PROTOCOL_VER, IS_STATUS, PBSE_SERVER_BUSY);
+
+      // don't let this get cleaned up below
+      chan = NULL;
       
       break;
       }
